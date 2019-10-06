9b8f7bbc2f3773e2401aaa6499d3da018fad3896
sahara-yeh <55381730+sahara-yeh@users.noreply.github.com>
2019-10-06 21:26:58 +0300

    Add files via upload

--- pre_sales_salest_team_agent/models/pre_sale_sale_team_agent.py
+++ pre_sales_salest_team_agent/models/pre_sale_sale_team_agent.py
@@ -18,6 +18,8 @@ class SaleOrderAgentSalesTeam(models.Model):
             for team in crm_team:
                 if team == self.type_id.sales_team_id:
                     newfilter = team.agent_ids
-            domain = {'agent_id_sales': [('id', 'in', sales_agent.ids),('id', 'in', newfilter.ids)]}
+            if !newfilter:
+                domain = {'agent_id_sales': [('id', 'in', sales_agent.ids)]}
+            else:
+                domain = {'agent_id_sales': [('id', 'in', sales_agent.ids),('id', 'in', newfilter.ids)]}
             return {'domain': domain}
-
