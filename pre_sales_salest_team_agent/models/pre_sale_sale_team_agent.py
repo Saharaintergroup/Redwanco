a32226b344df93d315edaeae2ef03eee2ce4da73
sahara-yeh <55381730+sahara-yeh@users.noreply.github.com>
2019-10-06 12:32:54 +0300

    Update pre_sale_sale_team_agent.py

--- pre_sales_salest_team_agent/models/pre_sale_sale_team_agent.py
+++ pre_sales_salest_team_agent/models/pre_sale_sale_team_agent.py
@@ -11,9 +11,9 @@ class SaleOrderAgentSalesTeam(models.Model):
     def onchange_partner__sales_ids(self):
         """Show sales agent based on customer zone and sales team"""
         if self.partner_id:
-            # commercial_line = self.env['commercial.line'].search([('commercial_line', 'in', self.partner_id.zone.id)])
-            # sales_agent = self.env['sales.agent'].search([('related_commercial_line', 'in', commercial_line.ids)])
-            sales_team_agents = self.env['sale.order'].search([('agent_id','in',self.team_id.agent_ids.ids)])
+            commercial_line = self.env['commercial.line'].search([('commercial_line', 'in', self.partner_id.zone.id)])
+            sales_agent = self.env['sales.agent'].search([('related_commercial_line', 'in', commercial_line.ids)])
+            sales_team_agents = self.env['sale.order'].search([('agent_id','in',self.sales_agent.ids)])
 
             domain = {'agent_id': [('id', 'in', sales_team_agents.ids)]}
             return {'domain': domain}
