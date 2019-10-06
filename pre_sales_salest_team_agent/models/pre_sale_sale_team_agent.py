5997adeeda7ab36a481a2a76a76b5632c2a416b2
sahara-yeh <55381730+sahara-yeh@users.noreply.github.com>
2019-10-06 13:35:48 +0300

    Update pre_sale_sale_team_agent.py

--- pre_sales_salest_team_agent/models/pre_sale_sale_team_agent.py
+++ pre_sales_salest_team_agent/models/pre_sale_sale_team_agent.py
@@ -5,7 +5,7 @@ class SaleOrderAgentSalesTeam(models.Model):
     _inherit = 'sale.order'
     _description = "Sales Agents in Sales Teams"
 
-    agent_id_sales = fields.Many2one('crm.team', string='Sales Agent')
+    agent_id_sales = fields.Many2one('sales.agent', string='Sales Agent')
 
     @api.onchange('partner_id','type_id')
     def onchange_partner__sales_ids(self):
@@ -13,8 +13,9 @@ class SaleOrderAgentSalesTeam(models.Model):
         if self.partner_id:
             commercial_line = self.env['commercial.line'].search([('commercial_line', 'in', self.partner_id.zone.id)])
             sales_agent = self.env['sales.agent'].search([('related_commercial_line', 'in', commercial_line.ids)])
-            sales_team_agents = self.env['crm.team'].search([('agent_id_sales','in',self.type_id.sales_team_id.agent_ids.ids)])
+            sales_team_agents = self.env['crm.team'].agent_ids;
+            sales_team_order_agents = self.env['sale.order'].search([('agent_id_sales','in',sales_team_agents.ids)])
 
-            domain = {'agent_id_sales': [('id', 'in', sales_team_agents.ids)]}
+            domain = {'agent_id_sales': [('id', 'in', sales_team_order_agents.ids)]}
             return {'domain': domain}
             # ('id', 'in', sales_agent.ids),
