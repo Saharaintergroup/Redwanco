dc76a287d40710f3d097af56de720f8b1ebc1535
sahara-yeh <55381730+sahara-yeh@users.noreply.github.com>
2019-10-06 06:05:15 +0300

    Update pre_sale_sale_team_agent.py

--- pre_sales_salest_team_agent/models/pre_sale_sale_team_agent.py
+++ pre_sales_salest_team_agent/models/pre_sale_sale_team_agent.py
@@ -8,13 +8,13 @@ class SaleOrderAgentSalesTeam(models.Model):
     agent_id = fields.Many2one('sales.agent', string='Sales Agent')
 
     @api.multi
-    @api.onchange('partner_id')
+    @api.onchange('partner_id','type_id')
     def onchange_partner_ids(self):
         """Show sales agent based on customer zone"""
         if self.partner_id:
             commercial_line = self.env['commercial.line'].search([('commercial_line', 'in', self.partner_id.zone.id)])
             sales_agent = self.env['sales.agent'].search([('related_commercial_line', 'in', commercial_line.ids)])
-            sales_teams = self.env['sale.order.type'].search([('sales_team_id','in',self.type_id.ids)])
+            sales_team_agents = self.env['crm.team'].search([('agent_id','in',self.agent_ids.ids)])
 
-            domain = {'agent_id': [('id', 'in', sales_agent.ids),('id', 'in', sales_teams.ids)]}
+            domain = {'agent_id': [('id', 'in', sales_team_agents.ids)]}
             return {'domain': domain}
