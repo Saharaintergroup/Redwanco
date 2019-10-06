68df1243adf28b707cbb9b889c03502c5474cfb2
sahara-yeh <55381730+sahara-yeh@users.noreply.github.com>
2019-10-06 18:07:03 +0300

    Update pre_sale_sale_team_agent.py

--- pre_sales_salest_team_agent/models/pre_sale_sale_team_agent.py
+++ pre_sales_salest_team_agent/models/pre_sale_sale_team_agent.py
@@ -6,16 +6,19 @@ class SaleOrderAgentSalesTeam(models.Model):
     _description = "Sales Agents in Sales Teams"
 
     agent_id_sales = fields.Many2one('sales.agent', string='Sales Agent')
-
+"""Show sales agent based on customer zone and sales team"""
     @api.onchange('partner_id','type_id')
-    def onchange_partner__sales_ids(self):
-        """Show sales agent based on customer zone and sales team"""
+    def onchange_partner_sales_ids(self):
         if self.partner_id:
             commercial_line = self.env['commercial.line'].search([('commercial_line', 'in', self.partner_id.zone.id)])
             sales_agent = self.env['sales.agent'].search([('related_commercial_line', 'in', commercial_line.ids)])
-            sales_team = self.env['sale.order'].search ([('type_id','in',self.type_id.sales_team_id.id)])
-            sales_team_order_agents = self.env['sales.agent'].search ([('related_sales_team','in',sales_team.sales_team_id.agent_ids.ids)])
+            domain = {'agent_id_sales': [('id', 'in', sales_agent.ids)]}
+            return {'domain': domain}
 
+    @api.onchange('type_id')
+    def onchange_type_sales_ids(self):
+        if self.type_id:
+            sales_team = self.env['sale.order.type'].search ([('sales_team_id','in',self.team_id.id)])
+            sales_team_order_agents = self.env['sales.agent'].search ([('related_sales_team','in',sales_team.ids)])
             domain = {'agent_id_sales': [('id', 'in', sales_team_order_agents)]}
             return {'domain': domain}
-            # ('id', 'in', sales_agent.ids),
