77811bc7dc85dbc6abef9627dbef9780792ea1eb
sahara-yeh <55381730+sahara-yeh@users.noreply.github.com>
2019-10-06 15:46:04 +0300

    Update pre_sale_sale_team_agent.py

--- pre_sales_salest_team_agent/models/pre_sale_sale_team_agent.py
+++ pre_sales_salest_team_agent/models/pre_sale_sale_team_agent.py
@@ -13,7 +13,7 @@ class SaleOrderAgentSalesTeam(models.Model):
         if self.partner_id:
             commercial_line = self.env['commercial.line'].search([('commercial_line', 'in', self.partner_id.zone.id)])
             sales_agent = self.env['sales.agent'].search([('related_commercial_line', 'in', commercial_line.ids)])
-            sales_team = self.env['sale.order.type'].search ([('sales_team_id','=',self.type_id.sales_team_id.id)])
+            sales_team = self.env['sale.order.type'].search ([('sales_team_id','=',self.type_id.sales_team_id)])
             sales_team_order_agents = self.env['sales.agent'].search ([('agent_id','in',sales_team.sales_team_id.agent_ids.ids)])
             # agent_list = []
             # teams = self.env['crm.team'].search([])
