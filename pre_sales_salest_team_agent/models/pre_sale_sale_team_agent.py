92213148e25584ec3d841c3e90c7fca146f45711
sahara-yeh <55381730+sahara-yeh@users.noreply.github.com>
2019-10-06 14:51:55 +0300

    Update pre_sale_sale_team_agent.py

--- pre_sales_salest_team_agent/models/pre_sale_sale_team_agent.py
+++ pre_sales_salest_team_agent/models/pre_sale_sale_team_agent.py
@@ -13,9 +13,15 @@ class SaleOrderAgentSalesTeam(models.Model):
         if self.partner_id:
             commercial_line = self.env['commercial.line'].search([('commercial_line', 'in', self.partner_id.zone.id)])
             sales_agent = self.env['sales.agent'].search([('related_commercial_line', 'in', commercial_line.ids)])
-#             sales_team_agents = self.env['crm.team'].search(['agent_ids','=',self.team_id.agent_ids.ids])
+            # sales_team_agents = self.env['crm.team'].search(['agent_ids','=',self.team_id.agent_ids.ids])
             sales_team_order_agents = self.env['sale.order'].search([('agent_id_sales','in',self.team_id.agent_ids.ids)])
+            agent_list = []
+            teams = self.env['crm.team'].search([])
+            for val in teams:
+                for agents in self.team_id.agent_ids:
+                    if agents.id == val.agents_ids.id:
+                        agent_list.append(val.id)
 
-            domain = {'agent_id_sales': [('id', 'in', sales_team_order_agents.ids)]}
+            domain = {'agent_id_sales': [('id', 'in', agent_list)]}
             return {'domain': domain}
             # ('id', 'in', sales_agent.ids),
