6e272420b4f6ca68abc545ad20be8fd4a6910d2e
sahara-yeh <55381730+sahara-yeh@users.noreply.github.com>
2019-10-06 17:40:24 +0300

    Update __manifest__.py

--- pre_sales/__manifest__.py
+++ pre_sales/__manifest__.py
@@ -8,7 +8,7 @@
         'maintainer': 'Cybrosys Techno Solutions',
         'company': 'Cybrosys Techno Solutions',
         'website': 'https://www.cybrosys.com',
-        'depends': ['base', 'contacts', 'sale', 'account', 'stock'],
+        'depends': ['base', 'contacts', 'sale', 'account', 'stock','sale_order_type'],
         'data': ['security/ir.model.access.csv',
                 'views/pre_sale.xml'
         ],
